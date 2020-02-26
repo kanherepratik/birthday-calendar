f28dab1e5ca99997d2f30549ee0bab869f9de331
Prateek Kanhere <prateek.kanhere@coverfoxmail.com>
2020-02-26 17:10:43 +0530

    clear cards on second click

--- index.js
+++ index.js
@@ -1,104 +1,102 @@
-document.onreadystatechange = () => {
-  if (document.readyState === "complete") {
-    let data = [];
-    const updateBtn = document.getElementById("update-btn");
-    updateBtn.addEventListener("click", () => {
-      data = JSON.parse(document.querySelector("#json-data").value);
-      const yearInputValue = parseInt(
-        document.getElementById("year-input").value
-      );
-      const result = filterDataByDay(data, yearInputValue);
-      fillBirthdays(result);
-    });
+let data = [];
+const updateBtn = document.getElementById("update-btn");
+const inputYearField = document.getElementById("year-input");
+let isUpdateClicked = false;
 
-    const fillBirthdays = birthdayData => {
-      Object.keys(birthdayData).forEach(day => {
-        const dayDiv = document.getElementById(day);
-        if (dayDiv.hasChildNodes()) {
-          dayDiv.innerHTML = "";
-          return false;
-        }
-        birthdayData[day].forEach(({ name }) => {
-          const boxDiv = document.createElement("div");
-          boxDiv.setAttribute("class", "box");
-          boxDiv.setAttribute(
-            "style",
-            `width:${getNumberOfBox(
-              birthdayData[day].length
-            )}%;height: ${getNumberOfBox(
-              birthdayData[day].length
-            )}%;background-color:${getRandomColor()}`
-          );
-          boxDiv.innerHTML = name;
-          dayDiv.appendChild(boxDiv);
-        });
-      });
-    };
-
-    // filter and prepare data by day
-    const filterDataByDay = (data, inputYear) => {
-      if (!data.length) {
-        return false;
-      }
-      const daysOfWeek = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
-      const requiredData = data
-        .filter(
-          ({ birthday }) => new Date(birthday).getFullYear() === inputYear
-        )
-        .reduce((acc, cur) => {
-          const date = new Date(cur.birthday).getDate();
-          const month = new Date(cur.birthday).getMonth() + 1;
-          const year = new Date(cur.birthday).getFullYear();
-          const dateObj = new Date(year, month - 1, date);
-          const day = daysOfWeek[dateObj.getDay()];
-          acc[day] = (acc[day] || []).concat({
-            name: cur.name
-              .split(" ")
-              .map(x => x[0].toUpperCase())
-              .join(""),
-            birthday: dateObj
-          });
-          return acc;
-        }, {});
-      return sortByAge(requiredData);
-    };
-
-    // sort the data in each day by age (youngest first)
-    const sortByAge = data => {
-      let resultData = {};
-      Object.keys(data).map(weekDay => {
-        resultData[weekDay] = data[weekDay].sort(
-          ({ birthday: ba }, { birthday: bb }) =>
-            new Date(bb).getTime() - new Date(ba).getTime()
-        );
-      });
-      return resultData;
-    };
-
-    const getNumberOfBox = n =>
-      100 / Math.sqrt(Math.pow(Math.ceil(Math.sqrt(n)), 2));
-    const getRandomColor = () => {
-      var lum = -0.25;
-      var hex = String(
-        "#" +
-          Math.random()
-            .toString(16)
-            .slice(2, 8)
-            .toUpperCase()
-      ).replace(/[^0-9a-f]/gi, "");
-      if (hex.length < 6) {
-        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-      }
-      var rgb = "#",
-        c,
-        i;
-      for (i = 0; i < 3; i++) {
-        c = parseInt(hex.substr(i * 2, 2), 16);
-        c = Math.round(Math.min(Math.max(0, c + c * lum), 255)).toString(16);
-        rgb += ("00" + c).substr(c.length);
-      }
-      return rgb;
-    };
-    // "#" + (((1 << 24) * Math.random()) | 0).toString(16);
+updateBtn.addEventListener("click", () => {
+  if (isUpdateClicked) {
+    clearBirthdayCards();
+    isUpdateClicked = false;
+    return false;
   }
+  data = JSON.parse(document.querySelector("#json-data").value);
+  const yearInputValue = parseInt(inputYearField.value);
+  const result = filterDataByDay(data, yearInputValue);
+  isUpdateClicked = true;
+  fillBirthdays(result);
+});
+
+const clearBirthdayCards = () => {
+  const boxContainerList = document.querySelectorAll(".inner");
+  for (let i = 0; i < boxContainerList.length; i++) {
+    boxContainerList[i].innerHTML = "";
+  }
+  inputYearField.value = "";
+};
+
+const fillBirthdays = birthdayData => {
+  Object.keys(birthdayData).forEach(day => {
+    const dayDiv = document.getElementById(day);
+    const fragment = document.createDocumentFragment();
+    // calculate number of boxes for perticular day
+    const requiredBoxes = getNumberOfBox(birthdayData[day].length);
+
+    // fill box with name and set width and height
+    for (let i = 0; i < requiredBoxes; i++) {
+      const boxDiv = document.createElement("div");
+      const boxSize = `${100 / Math.sqrt(requiredBoxes)}%`;
+      const boxBackground = `${getRandomColor()}`;
+      boxDiv.setAttribute("class", "box");
+      if (birthdayData[day][i]) {
+        boxDiv.setAttribute(
+          "style",
+          `background-color:${boxBackground}; width:${boxSize};height:${boxSize}`
+        );
+        boxDiv.innerHTML = birthdayData[day][i].name;
+      } else {
+        boxDiv.setAttribute("style", `width:${boxSize};height:${boxSize}`);
+      }
+      fragment.appendChild(boxDiv);
+    }
+    dayDiv.appendChild(fragment);
+  });
+};
+
+// filter and prepare data by day
+const filterDataByDay = (data, inputYear) => {
+  if (!data.length && !inputYear) {
+    return false;
+  }
+  const daysOfWeek = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+  const requiredData = data
+    .filter(({ birthday }) => new Date(birthday).getFullYear() === inputYear)
+    .reduce((acc, cur) => {
+      const date = new Date(cur.birthday).getDate();
+      const month = new Date(cur.birthday).getMonth() + 1;
+      const year = new Date(cur.birthday).getFullYear();
+      const dateObj = new Date(year, month - 1, date);
+      const day = daysOfWeek[dateObj.getDay()];
+      acc[day] = (acc[day] || []).concat({
+        name: cur.name
+          .split(" ")
+          .map(x => x[0].toUpperCase())
+          .join(""),
+        birthday: dateObj
+      });
+      return acc;
+    }, {});
+  return sortByAge(requiredData);
+};
+
+// sort the data in each day by age (youngest first)
+const sortByAge = data => {
+  let resultData = {};
+  Object.keys(data).map(weekDay => {
+    resultData[weekDay] = data[weekDay].sort(
+      ({ birthday: firstBirthday }, { birthday: secondBirthday }) =>
+        new Date(secondBirthday).getTime() - new Date(firstBirthday).getTime()
+    );
+  });
+  return resultData;
+};
+
+// get number of box to be rendered for each day
+const getNumberOfBox = n => Math.pow(Math.ceil(Math.sqrt(n)), 2);
+
+// generate a random color code
+const getRandomColor = () => {
+  const h = Math.floor(Math.random() * 360),
+    s = Math.floor(Math.random() * 100) + "%",
+    l = Math.floor(Math.random() * 60) + "%";
+  return `hsl(${h},${s},${l})`;
 };
